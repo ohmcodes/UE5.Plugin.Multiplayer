f42cc9652f69b955f38b6fc828e63bd70b3c16fb
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2026-09-22 23:59:27 -0700

    Sec2 Lec18

--- MPPlugin58/Plugins/MultiplayerSessionsUE58/Source/MultiplayerSessionsUE58/MultiplayerSessionsUE58.Build.cs
+++ MPPlugin58/Plugins/MultiplayerSessionsUE58/Source/MultiplayerSessionsUE58/MultiplayerSessionsUE58.Build.cs
@@ -28,6 +28,9 @@ public class MultiplayerSessionsUE58 : ModuleRules
 				"Core",
 				"OnlineSubsystem",
 				"OnlineSubsystemSteam",
+				"UMG",
+				"Slate",
+                "SlateCore",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
